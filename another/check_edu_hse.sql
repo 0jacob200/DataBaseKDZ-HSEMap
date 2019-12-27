993be328abfc1ac07e4f2e302c488f5fda2efb79
0jacob200 <50717905+0jacob200@users.noreply.github.com>
2019-12-28 00:28:58 +0300

    триггер сделан

--- another/check_edu_hse.sql
+++ another/check_edu_hse.sql
@@ -1,8 +1,9 @@
 --если триггер работает верно то в двух таблицах не должно быть кортежей (данные введены неверно)
 insert into [dbo].[Professor]
-values ('Профессор', 'Мария', 'Георгиевна', 'Борева', 'ntgqucasvaegm@hse.ru')
+values ('Профессор', 'Мария', 'Георгиевна', 'Борева', 'ntgqucasvaegm@he.ru'),
+('Профессор', 'Остап', 'Куприянович', 'Киреев', 'iyxzdvmahkihse.ru')
 
-select * from [dbo].[Professor] where Professor_Email = 'ntgqucasvaegm@hse.ru'
+select * from [dbo].[Professor] where Professor_Email = 'ntgqucasvaegm@he.ru'
 go
 
 --waitfor delay '00:00:05'
@@ -10,5 +11,4 @@ go
 delete from [dbo].[Professor]
 where Professor_Email= 'ntgqucasvaegm@hse.ru'
 
-select * from [dbo].[Student] where Student_Email= 'ntgqucasvaegm@hse.ru'
-
+select * from [dbo].[Student] where Student_Email= 'ntgqucasvaegm@he.ru'
--- scripts/bd_hsemap_edu_triggers.sql
+++ scripts/bd_hsemap_edu_triggers.sql
@@ -5,22 +5,19 @@ create trigger Email_Student_Edu_hse_ru on [dbo].[Student]
 	instead of insert
 as
 begin
-	declare @email varchar(64);
-	declare @firstN varchar(64);
-	declare @secondN varchar(64);
-	declare @surname varchar(128);
+	--declare @email varchar(64);
+	--declare @firstN varchar(64);
+	--declare @secondN varchar(64);
+	--declare @surname varchar(128);
 
-	set @email = (select Student_Email from inserted)
-	set @firstN = (select First_Name from inserted)
-	set @secondN = (select Second_Name from inserted)
-	set @surname = (select Surname from inserted)
-
-	if (@email like '%__@edu.hse.ru') and
-	(@surname like '__%') and (@firstN like '__%') and (@secondN like '__%')
-		begin
-			insert into [dbo].[Student]
-			values (@firstN, @secondN, @surname, @email)
-		end
+	--set @email = (select Student_Email from inserted)
+	--set @firstN = (select First_Name from inserted)
+	--set @secondN = (select Second_Name from inserted)
+	--set @surname = (select Surname from inserted)
+	insert into Student
+	select * from inserted where 
+	(Student_Email like '%__@edu.hse.ru') and
+	(Surname like '__%') and (First_Name like '__%') and (Second_Name like '__%')
 end
 go
 
@@ -28,30 +25,10 @@ create trigger Email_Professor_hse_ru on [dbo].[Professor]
 	instead of insert
 as
 begin
-	declare @email varchar(64);
-	declare @firstN varchar(64);
-	declare @secondN varchar(64);
-	declare @surname varchar(128);
-	declare @rank varchar(64);
-
-	set @email = (select Professor_Email from inserted)
-	set @firstN = (select First_Name from inserted)
-	set @secondN = (select Second_Name from inserted)
-	set @surname = (select Surname from inserted)
-	set @rank = (select [Rank] from inserted)
-
-	if (@email like '%__@hse.ru') and
-	(@surname like '__%') and (@firstN like '__%') and (@secondN like '__%')
-		begin
-			if (@rank = 'Старший преподаватель')
-			or (@rank = 'Тьютор') 
-			or (@rank = 'Доцент') 
-			or (@rank = 'Профессор')
-			or (@rank = 'Приглашённый преподаватель') 
-			or (@rank = 'Преподаватель') 
-			begin
-				insert into [dbo].[Professor]
-				values (@rank, @firstN, @secondN, @surname, @email)
-			end
-		end
+	insert into [dbo].[Professor]
+	select * from inserted where 
+	(Professor_Email like '%__@hse.ru') and
+	(Surname like '__%') and (First_Name like '__%') and (Second_Name like '__%') and
+	([Rank] in ('Старший преподаватель','Тьютр','Доцент','Профессор',
+	'Приглашенный преподаватель','Преподаватель'))
 end
