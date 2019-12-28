84e7c54ecb34c807ea146b9edea94098481ce2fb
0jacob200 <50717905+0jacob200@users.noreply.github.com>
2019-12-28 04:16:03 +0300

    группировка

---
+++ scripts/functions/bd_hsemap_edu_functions.sql
@@ -0,0 +1,27 @@
+USE hsemap_db_edu
+go
+
+CREATE FUNCTION SearchForClasses (@Email varchar(64), @Date date)
+RETURNS TABLE
+AS
+RETURN
+(
+	SELECT ID_Time_span, Campus_Name, Building_Name, Room_Number FROM Groups_Class WHERE (Full_Group_Number
+	in (SELECT Full_Group_Number FROM Groups_Student WHERE Student_Email = @Email))
+	and Date_Class = @Date
+
+	UNION
+
+	SELECT ID_Time_span, Campus_Name, Building_Name, Room_Number FROM Student_Addition_Class WHERE Student_Email = @Email
+	and Date_Class = @Date
+);
+go
+
+CREATE FUNCTION FindRoom (@StartRoomNumber varchar(16))
+RETURNS TABLE
+AS
+RETURN
+(
+	SELECT Building_Name, Room_Number FROM Room
+	WHERE Room_Number LIKE (@StartRoomNumber + '%')
+);
