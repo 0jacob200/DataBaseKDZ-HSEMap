2c84bd2015f31b4e8fa699fd1692e0486bcafb79
0jacob200 <50717905+0jacob200@users.noreply.github.com>
2019-12-29 15:31:42 +0300

    перегруппировка функций

--- scripts/procedures/procedure2.sql
+++ scripts/procedures/procedure2.sql
@@ -1,19 +1,5 @@
-Create or alter procedure InsertNewStudentWithGroup
-(@email varchar(64),
-@firstName nvarchar(64),
-@secondName nvarchar(64),
-@surname nvarchar(128),
-@fullGroupNumber nvarchar(16)) as
-begin
-	if @email not in (select Student_Email from Student) 
-	and @fullGroupNumber in (select Full_Group_Number from Groups)
-	begin
-		insert into Student values (@firstName, @secondName, @surname, @email);
-		insert into Groups_Student values (@fullGroupNumber, @email);
-	end;
-	else
-		print('Не получилось добавить студента')
-end;
+use hsemap_db_edu
+go
 
 Create or alter procedure AddClassForGroup
 (@startTime time(0),
