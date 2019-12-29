9e741af2c525e1a14f341f099c9e80ae003383df
Pr0ph3tv <Prophet.reaper@yandex.ru>
2019-12-29 19:33:37 +0300

    Добавление названия группы в ScheduleView

--- scripts/views/ScheduleView.sql
+++ scripts/views/ScheduleView.sql
@@ -2,11 +2,14 @@ use hsemap_db_edu
 go
 
 CREATE VIEW Schedule
-AS SELECT Start_Time, End_Time, Campus_Name, Room_Number, Date_Class, DATENAME(dw, Date_Class) AS Week_Day, Name_of_subject,
+AS SELECT Start_Time, End_Time, Class.Campus_Name, Class.Room_Number, 
+Class.Date_Class, DATENAME(dw, Class.Date_Class) AS Week_Day, Full_Group_Number, Name_of_subject,
  CONCAT(Surname, ' ', First_Name,' ', Second_Name) AS FSS
-FROM Class, Time_span, Professor
+FROM Class, Time_span, Professor, Groups_Class
 WHERE
 Class.ID_Time_span = Time_span.ID_Time_span
 and
 Professor.Professor_Email = Class.Professor_Email
+and
+Groups_Class.ID_Time_span = Class.ID_Time_span
 
