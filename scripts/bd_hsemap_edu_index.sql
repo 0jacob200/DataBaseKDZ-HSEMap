9c1c6e4e6cfc1579419143b6cb0ed40f5515d906
0jacob200 <50717905+0jacob200@users.noreply.github.com>
2020-01-03 20:58:50 +0300

    Update bd_hsemap_edu_index.sql

--- scripts/bd_hsemap_edu_index.sql
+++ scripts/bd_hsemap_edu_index.sql
@@ -1,5 +1,5 @@
 use hsemap_db_edu
 go
 
-create NONCLUSTERED index Student_Index on Student (Surname, First_Name, Second_Name)
+create UNIQUE NONCLUSTERED index Professor_Index on Professor (Professor_Email)
 go
