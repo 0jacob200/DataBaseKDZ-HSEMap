8bc0ca77d4213462cd119eb8e11345b59382f402
0jacob200 <50717905+0jacob200@users.noreply.github.com>
2020-01-05 20:25:19 +0300

    названия

--- scripts/functions/FindRoomFunction.sql
+++
@@ -1,8 +0,0 @@
-CREATE FUNCTION FindRoom (@StartRoomNumber varchar(16))
-RETURNS TABLE
-AS
-RETURN
-(
-	SELECT Building_Name, Room_Number FROM Room
-	WHERE Room_Number LIKE (@StartRoomNumber + '%')
-);
